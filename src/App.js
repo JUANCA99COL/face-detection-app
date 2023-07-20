208595b31b7ed41f4568e434dbc04b32f3f0f7fa
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2023-07-20 21:26:58 +1200

    updating link fetch

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://login-app-api-ap19.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
