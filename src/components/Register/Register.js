62c92e89737237f61013de0ce3ba6458c989cb64
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2023-07-30 15:52:12 +1200

    bg update style

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -43,33 +43,33 @@ class Register extends React.Component {
 
     render () {
         return  (
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="bg-purple br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
             <main className="pa4 black-80">
                 <div className="measure">
                     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                     <legend className="f1 fw6 ph0 mh0">REGISTER</legend>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                        <label className="db white fw6 lh-copy f6" htmlFor="name">Name</label>
                         <input 
-                            className="shadow-5 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            className="shadow-5 pa2 input-reset ba bg-white hover-bg-black hover-white w-100" 
                             type="text" 
                             name="name"  
                             id="name"
                             onChange={this.onNameChange}/>
                     </div>
                     <div className="mt3">
-                        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                        <label className="db fw6 white lh-copy f6" htmlFor="email-address">Email</label>
                         <input 
-                            className="shadow-5 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            className="shadow-5 pa2 input-reset ba bg-white hover-bg-black hover-white w-100" 
                             type="email" 
                             name="email-address"  
                             id="email-address"
                             onChange={this.onEmailChange}/>
                     </div>
                     <div className="mv3">
-                        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                        <label className="db fw6 white lh-copy f6" htmlFor="password">Password</label>
                         <input 
-                            className="shadow-5 b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            className="shadow-5 b pa2 input-reset ba bg-white hover-bg-black hover-white w-100" 
                             type="password" 
                             name="password"  
                             id="password"
@@ -79,7 +79,7 @@ class Register extends React.Component {
                     <div className="">
                         <input 
                             onClick={this.onSubmitSignIn}
-                            className="b ph3 pv2 pointer input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            className="b ph3 pv2 pointer input-reset ba b--black bg-white grow pointer f6 dib" 
                             type="submit" 
                             value="Register"/>
                         </div>
--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -37,21 +37,21 @@ class Signin extends React.Component {
 
     render( ) {
         return (
-            <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="br3 bg-purple ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
                         <legend className="f1 fw6 ph0 mh0">SIGN IN</legend>
                         <div className="mt3">
-                            <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                            <label className="db fw6 white lh-copy f6" htmlFor="email-address">Email</label>
                             <input 
                             onChange={this.onEmailChange} 
-                            className="shadow-5 pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
+                            className="shadow-5 pa2 input-reset ba bg-white hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address"/>
                         </div>
                         <div className="mv3">
-                            <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                            <label className="db fw6 white lh-copy f6" htmlFor="password">Password</label>
                             <input 
-                            className="shadow-5 b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+                            className="shadow-5 b pa2 input-reset ba bg-white hover-bg-black hover-white w-100" 
                             type="password" 
                             name="password"  
                             id="password"
@@ -60,7 +60,7 @@ class Signin extends React.Component {
                         </fieldset>
                         <div className="">
                         <input 
-                            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                            className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib" 
                             type="submit" 
                             value="Sign in"
                             onClick={this.onSubmitSignIn}/>
@@ -68,7 +68,7 @@ class Signin extends React.Component {
                         <div className="lh-copy mt3">
                          <p 
                             onClick={() => this.props.onRouteChange('register')}
-                            className="pointer f6 link dim black db">Register
+                            className="pointer f6 link dim white db">Register
                          </p>
                         </div>
                     </div>
