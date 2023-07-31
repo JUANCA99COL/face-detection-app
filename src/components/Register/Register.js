23298c98c287f7a684ab2fef4421a4880a333e83
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2023-07-31 17:19:15 +1200

    style contact color change

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -79,7 +79,7 @@ class Register extends React.Component {
                     <div className="">
                         <input 
                             onClick={this.onSubmitSignIn}
-                            className="b ph3 pv2 pointer input-reset ba b--black bg-white grow pointer f6 dib" 
+                            className="b ph3 pv2 pointer white input-reset ba b--black bg-black grow pointer f6 dib" 
                             type="submit" 
                             value="Register"/>
                         </div>
--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -60,7 +60,7 @@ class Signin extends React.Component {
                         </fieldset>
                         <div className="">
                         <input 
-                            className="b ph3 pv2 input-reset ba b--black bg-white grow pointer f6 dib" 
+                            className="b ph3 pv2 input-reset ba b--black bg-black white grow pointer f6 dib" 
                             type="submit" 
                             value="Sign in"
                             onClick={this.onSubmitSignIn}/>
