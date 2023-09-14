efa3c3e92b596445ce5b5ae96db178fe88fcf333
JUANCA99COL <jcamilocarorodriguez@gmail.com>
2023-09-14 19:34:02 +1200

    adding opacity

--- src/components/SignIn/Signin.js
+++ src/components/SignIn/Signin.js
@@ -38,7 +38,7 @@ class Signin extends React.Component {
 
     render( ) {
         return (
-            <article className="br3 bg-purple ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+            <article className="re-form br3 bg-purple ba b--white-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
                 <main className="pa4 black-80">
                     <div className="measure">
                         <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
